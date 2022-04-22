dacbd56e30903662ca6735f5c73c69ef5865e4a2
bnwong2 <72638022+bnwong2@users.noreply.github.com>
2022-04-21 20:52:41 -0700

    Create CustomerView.java

---
+++ src/CustomerView.java
@@ -0,0 +1,17 @@
+//import required classes and packages  
+import javax.swing.*;  
+import java.awt.*;  
+  
+//create NewPage class to create a new page on which user will navigate  
+class CustomerView extends JFrame  
+{  
+    //constructor  
+    CustomerView()  
+    {  
+        setDefaultCloseOperation(javax.swing.  
+        WindowConstants.DISPOSE_ON_CLOSE);  
+        setTitle("Welcome");  
+        setSize(400, 200);  
+    }  
+    
+}  
