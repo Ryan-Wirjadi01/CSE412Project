8c8fb15d48a10e57434006d2e72be09bd8fb58ac
jlforsy1 <jlforsy1@asu.edu>
2022-04-24 15:49:16 -0700

    methods for accessing customer, restaurant, and food info.

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -5,75 +5,78 @@ import java.util.ArrayList;
 public class DatabaseConnection {
 
     //change Url, User, and Pass depending on what it is for your own postgresql server
-    private static final String connectionUrl = "jdbc:postgresql://localhost:5432/foodApp"; 	//****IMPORTANT: Change this*****
+    private static final String connectionUrl = "jdbc:postgresql://localhost:5432/foodApp";    //****IMPORTANT: Change this*****
     private static final String user = "postgres";
-    private static final String pass = "password";												//****IMPORTANT: Change this*****
+    private static final String pass = "password";                                                //****IMPORTANT: Change this*****
 
     //testing methods
-	/*
-	 * public static void main(String[] args) { //ArrayList<String> res =
-	 * getRestaurants("tempe"); //System.out.println("length: " + res.size()); //for
-	 * (String str: res) { // System.out.println(str); //}
-	 * 
-	 * //ArrayList<String> food = getFoodList("domino's");
-	 * //System.out.println("length: " + food.size()); //for (String str: food) { //
-	 * System.out.println(str); //}
-	 * 
-	 * //System.out.println("Price: " + getFoodPrice("domino's", "cheese pizza"));
-	 * 
-	 * //insertCustomer(23456789, "Tempe", "First Last", 4);
-	 * 
-	 * //System.out.println("Customer name: " + getCustomerName(3));
-	 * //System.out.println("Customer address: " + getCustomerAddress(3));
-	 * 
-	 * //insertOrder("not delivered", 4, 4, 4, 3, "2022-23-04 21:22:15", 2, 2,
-	 * 17.0); //updateOrderStatus(4, "not delivered");
-	 * 
-	 * ArrayList<String> id = getDeliveryDriverID(); for(String i : id) {
-	 * System.out.println(i); } }
-	 */
-    
-    public ArrayList<String> getDeliveryDriverID() {
-    	Connection c;
-    	Statement stmt;
-    	ArrayList<String> deliveryID = new ArrayList<>();
-    	try {
-    		Class.forName("org.postgresql.Driver");
-    		c = DriverManager.getConnection(connectionUrl, user, pass);
-    		c.setAutoCommit(false);
-    		
-    		stmt = c.createStatement();
-    		ResultSet rs = stmt.executeQuery("SELECT did FROM deliveryDriver;");
-    		
-    		while(rs.next()) {
-			deliveryID.add(rs.getString("did"));
-    		}
-    		rs.close();
-    		stmt.close();
-    		c.close();
-    	} catch(Exception e) {
-    		System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+    /*public static void main(String[] args) {
+        ArrayList<String> res = getRestaurants("tempe");
+        System.out.println("length: " + res.size());
+        for (String str : res) {
+            System.out.println(str);
+        }
+        ArrayList<String> food = getFoodList("domino's");
+        System.out.println("length: " + food.size());
+        for (String str : food) {
+            System.out.println(str);
+        }
+        System.out.println("Price: " + getFoodPrice("domino's", "cheese pizza"));
+
+        insertCustomer(23456789, "Tempe", "First Last", 4);
+
+        System.out.println("Customer name: " + getCustomerName(3));
+        System.out.println("Customer address: " + getCustomerAddress(3));
+
+        insertOrder("not delivered", 4, 4, 4, 3, "2022-23-04 21:22:15", 2, 2, 17.0);
+        updateOrderStatus(4, "not delivered");
+
+        ArrayList<String> id = getDeliveryDriverID();
+        for (String i : id) {
+            System.out.println(i);
+        }
+    }*/
+
+    public static ArrayList<String> getDeliveryDriverID() {
+        Connection c;
+        Statement stmt;
+        ArrayList<String> deliveryID = new ArrayList<>();
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection(connectionUrl, user, pass);
+            c.setAutoCommit(false);
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT did FROM deliveryDriver;");
+
+            while (rs.next()) {
+                deliveryID.add(rs.getString("did"));
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
-    	}
-    	return deliveryID;
+        }
+        return deliveryID;
     }
 
-
-    public static ArrayList<String> getRestaurants(String city){
+    public static ArrayList<String> getRestaurants(String city) {
         Connection c;
         Statement stmt;
         ArrayList<String> restaurants = new ArrayList<>();
-        try{
+        try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection(connectionUrl,user,pass);
+            c = DriverManager.getConnection(connectionUrl, user, pass);
             c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
+            //System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery("select name from restaurant where UPPER(location) LIKE UPPER('%" + city + "%');");
 
-            while(rs.next()){
-               String str = rs.getString("name");
+            while (rs.next()) {
+                String str = rs.getString("name");
 
                 restaurants.add(str);
             }
@@ -81,30 +84,30 @@ public class DatabaseConnection {
             stmt.close();
             c.close();
 
-        } catch(Exception e){
-            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
 
         return restaurants;
     }
 
-    public static ArrayList<String> getFoodList(String restaurant){
+    public static ArrayList<String> getFoodList(String restaurant) {
         restaurant = restaurant.replace('\'', '_');
         Connection c;
         Statement stmt;
         ArrayList<String> foods = new ArrayList<>();
 
-        try{
+        try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection(connectionUrl,user,pass);
+            c = DriverManager.getConnection(connectionUrl, user, pass);
             c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
+            //System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("select CF.name from cook_food as CF, restaurant as R WHERE CF.rid = R.rid and UPPER(R.name) like UPPER('%" + restaurant +"%');");
+            ResultSet rs = stmt.executeQuery("select CF.name from cook_food as CF, restaurant as R WHERE CF.rid = R.rid and UPPER(R.name) like UPPER('%" + restaurant + "%');");
 
-            while(rs.next()){
+            while (rs.next()) {
                 String str = rs.getString("name");
 
                 foods.add(str);
@@ -113,53 +116,53 @@ public class DatabaseConnection {
             stmt.close();
             c.close();
 
-        } catch(Exception e){
-            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
 
         return foods;
     }
 
-    public static float getFoodPrice(String restaurant, String food){
+    public static float getFoodPrice(String restaurant, String food) {
         restaurant = restaurant.replace('\'', '_');
         Connection c;
         Statement stmt;
         float price = 0;
 
-        try{
+        try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection(connectionUrl,user,pass);
+            c = DriverManager.getConnection(connectionUrl, user, pass);
             c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
+            //System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery("select CF.price " +
                     "FROM cook_food as CF, restaurant as R " +
-                    "WHERE CF.rid = R.rid and UPPER(CF.name) like UPPER('" + food + "') and UPPER(R.name) like UPPER('%" + restaurant +"%');");
-            while(rs.next()) {
+                    "WHERE CF.rid = R.rid and UPPER(CF.name) like UPPER('" + food + "') and UPPER(R.name) like UPPER('%" + restaurant + "%');");
+            while (rs.next()) {
                 price = rs.getFloat("price");
             }
             rs.close();
             stmt.close();
             c.close();
 
-        } catch(Exception e){
-            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
 
         return price;
     }
 
-    public static void insertCustomer(int payInfo, String address, String name, int id){
+    public static void insertCustomer(int payInfo, String address, String name, int id) {
         Connection c;
         Statement stmt;
-        try{
+        try {
             Class.forName("org.postgresql.Driver");
             c = DriverManager.getConnection(connectionUrl, user, pass);
             c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
+            //System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
             stmt.executeUpdate("INSERT INTO CUSTOMER (pay_information, address, name, cid) "
@@ -167,20 +170,20 @@ public class DatabaseConnection {
             stmt.close();
             c.commit();
             c.close();
-        }catch(Exception e){
-            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
         System.out.println("Records created successfully");
     }
 
-    public static String getCustomerName(int cid){
+    public static String getCustomerName(int cid) {
         Connection c;
         Statement stmt;
         String name = "";
         try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection(connectionUrl,user,pass);
+            c = DriverManager.getConnection(connectionUrl, user, pass);
             c.setAutoCommit(false);
             stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT name FROM customer WHERE cid = " + cid + ";");
@@ -189,20 +192,20 @@ public class DatabaseConnection {
             rs.close();
             stmt.close();
             c.close();
-        } catch( Exception e ) {
+        } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
         return name;
     }
 
-    public static String getCustomerAddress(int cid){
+    public static String getCustomerAddress(int cid) {
         Connection c;
         Statement stmt;
         String address = "";
         try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection(connectionUrl,user,pass);
+            c = DriverManager.getConnection(connectionUrl, user, pass);
             c.setAutoCommit(false);
             stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT address FROM customer WHERE cid = " + cid + ";");
@@ -211,17 +214,19 @@ public class DatabaseConnection {
             rs.close();
             stmt.close();
             c.close();
-        } catch( Exception e ) {
+        } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
         return address;
     }
 
-    public static void insertOrder(String status, int rest_rating, int drive_rating, int order_id, int food_rating, String timeStamp, int customer_id, int quantity, double price){
+    //public static ArrayList<Integer>
+
+    public static void insertOrder(String status, int rest_rating, int drive_rating, int order_id, int food_rating, String timeStamp, int customer_id, int quantity, double price) {
         Connection c;
         Statement stmt;
-        try{
+        try {
             Class.forName("org.postgresql.Driver");
             c = DriverManager.getConnection(connectionUrl, user, pass);
             c.setAutoCommit(false);
@@ -232,18 +237,18 @@ public class DatabaseConnection {
             stmt.close();
             c.commit();
             c.close();
-        }catch(Exception e){
-            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
     }
 
-    public static void updateOrderStatus(int orderID, String status){
+    public static void updateOrderStatus(int orderID, String status) {
         Connection c;
         Statement stmt;
-        try{
+        try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection(connectionUrl,user,pass);
+            c = DriverManager.getConnection(connectionUrl, user, pass);
             c.setAutoCommit(false);
 
             stmt = c.createStatement();
@@ -251,8 +256,8 @@ public class DatabaseConnection {
             stmt.close();
             c.commit();
             c.close();
-        }catch(Exception e){
-            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
     }
